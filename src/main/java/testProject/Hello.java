7b2a8ab4b642739c2145eb1ff41d21e371c3d3c1
nichaos2 <nick.ioannou86@gmail.com>
2018-08-08 11:32:20 +0200

    second branch commit

--- src/main/java/testProject/Hello.java
+++ src/main/java/testProject/Hello.java
@@ -3,7 +3,7 @@ package testProject;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GIT");
+		System.out.println("Hello GIT in Eclipse helps?");
 	}
 
 }
