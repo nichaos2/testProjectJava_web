24ec140aefc5f8fac01da3d76f00dca59f4becf6
nichaos2 <nick.ioannou86@gmail.com>
2018-08-21 17:13:51 +0200

    serviceMaterial and Material Generator

--- src/main/java/service/IMaterialService.java
+++ src/main/java/service/IMaterialService.java
@@ -1,9 +1,12 @@
 package service;
 
+import java.util.List;
+
 import model.Material;
 
 public interface IMaterialService {
 
 	public List<Material> getAllMaterials();
 
+	public List<Material> addMaterialToList(Material m);
 }
