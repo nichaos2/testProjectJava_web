107efaf9e4a219ea178ddf8b414c5f17cc9b40a2
nichaos2 <nick.ioannou86@gmail.com>
2018-08-22 09:54:07 +0200

    retry commit

--- src/main/java/testProject/Hello.java
+++ src/main/java/testProject/Hello.java
@@ -3,7 +3,7 @@ package testProject;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GIT in Eclipse helps?");
+		System.out.println("Hello GIT in Eclipse helps? I going to retry");
 	}
 
 }
